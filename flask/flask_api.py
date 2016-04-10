71d2846c5ec02f86342cc4635887a6cc6147c001
spoid <spoidbart@gmail.com>
2016-04-10 11:01:17 +0200

    Added Prototype Flask Backend

---
+++ flask/flask_api.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+#sys.setdefaultencoding('utf-8')
+
+import ConfigParser
+import os
+import sys
+from flask import Flask
+import tweepy
+import json
+
+
+##read API access tokens
+configParser = ConfigParser.RawConfigParser()   
+configFilePath = os.path.abspath(os.path.dirname(sys.argv[0])) + '/auth.cfg'
+configParser.read(configFilePath)
+
+CONSUMER_KEY = configParser.get('Twitter', 'CONSUMER_KEY')
+CONSUMER_SECRET = configParser.get('Twitter', 'CONSUMER_SECRET')
+ACCESS_KEY = configParser.get('Twitter', 'ACCESS_KEY')
+ACCESS_SECRET = configParser.get('Twitter', 'ACCESS_SECRET')
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+
+app = Flask(__name__)
+
+
+def list_of_srobjects_to_dictionary(list_of_srobjects):
+    dictionary = {}
+    for sr_object in list_of_srobjects:
+        dictionary[sr_object.id] = {
+            'created_at': str(sr_object.created_at),
+            'retweet_count': sr_object.retweet_count,
+            'favorite_count': sr_object.favorite_count,
+            'author_screen_name': sr_object.author.screen_name,
+            'author_followers_count': sr_object.author.followers_count,
+            'author_friends_count': sr_object.author.friends_count,
+            'text': sr_object.text,
+            'score': sr_object.retweet_count + sr_object.favorite_count,
+            'score_divided': (float(sr_object.retweet_count) + float(sr_object.favorite_count) + 1) / float(sr_object.author.followers_count)
+        }
+    return dictionary
+    
+    
+@app.route('/')
+@app.route('/index')
+def index():
+    return """
+    <html><header></header><body>
+    <h1><a href="/upload">Placeholder</a></h1>
+    </body></html>
+    """
+
+
+@app.route('/get_tweets_from_hashtag/<string:hashtag>', methods=['GET', 'POST'])
+def get_tweets_from_hashtag(hashtag):
+    #turn pepe+football query into #pepe #football
+    hashtag = "#" + hashtag.replace("+"," #")
+    query = hashtag #space separate for AND
+    max_tweets = 500 #very few ..
+
+    print "started query..."
+    #, sort_by="retweet_count-asc"
+    results = [status for status in tweepy.Cursor(api.search, q=query, languages=["german"]).items(max_tweets)]
+    print "query done..."
+    
+    dictionary = list_of_srobjects_to_dictionary(results)
+    sorted_list = sorted(dictionary.iteritems(), key=lambda (x, y): y['score'], reverse=True) #
+    jayson = json.dumps(sorted_list)
+    
+    return jayson
+    
+
+
+
+
+#LIVE SCRAPER
+
+app.run(debug=True, host='0.0.0.0', port=8006)
---
+++ flask/live_scraper.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+#sys.setdefaultencoding('utf-8')
+
+
+##Scrapes Twitter Livestream to a file, periodically [~15 Minutes]
+##runs word2vec via Gensim for Hashtag Prediction from Raw Tweet Text.
+
+import ConfigParser
+import os
+import sys
+from flask import Flask
+import tweepy
+import json
+import re
+from threading import Thread
+from tweepy.streaming import StreamListener
+import traceback
+import time
+import sys
+import datetime as dt
+from tweepy import Stream
+import gensim
+
+##read API access tokens
+configParser = ConfigParser.RawConfigParser()   
+configFilePath = os.path.abspath(os.path.dirname(sys.argv[0])) + '/auth.cfg'
+configParser.read(configFilePath)
+
+CONSUMER_KEY = configParser.get('Twitter', 'CONSUMER_KEY')
+CONSUMER_SECRET = configParser.get('Twitter', 'CONSUMER_SECRET')
+ACCESS_KEY = configParser.get('Twitter', 'ACCESS_KEY')
+ACCESS_SECRET = configParser.get('Twitter', 'ACCESS_SECRET')
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+
+
+hashtag_finder = re.compile('(?:(?<=\s)|^)#(\w*[A-Za-z_]+\w*)')
+
+class listener(StreamListener):
+
+    def __init__(self):
+        super(StreamListener, self).__init__()
+        data = []
+        filer = open(scrape1.txt, 'wb')        
+        
+        pass
+
+    def get_user_data(self, data):
+        user = {}
+        user['followers'] = data['user']['followers_count']
+        user['friends'] = data['user']['friends_count']
+        user['img'] = data['user']['profile_image_url_https']
+        user['name'] = data['user']['name']
+        user['screenname'] = data['user']['screen_name']
+        user['bio'] = data['user']['description']
+        return user
+        
+    def on_data(self, data):
+        
+        try:        
+            if data:
+                data = json.loads(data)
+            #t0 = time.time()    
+            
+            if 'user' in data:
+                
+                if data['user']['followers_count'] > 0:
+                    #print data['text']
+                    matches = hashtag_finder.findall(data['text'])
+                    if len(matches) > 0: 
+                        print  matches
+                        
+            #print time.time() - t0db.create
+        except: 
+            print traceback.format_exc()
+            #db.log(traceback.format_exc(),'stream')
+            #db.log(sys.exc_info()[0],'stream')
+            #c.shutdown()           
+        
+        return True
+
+
+    def on_error(self, status):
+        print status
+                        
+class StreamingScraper(Thread):
+    def __init__(self):
+        super(StreamingScraper, self).__init__()
+        self.daemon = True
+        self.cancelled = False
+        
+    def run(self):      
+        while True:
+            try: 
+                twitterStream = Stream(auth, listener())
+                twitterStream.filter(locations=[-180,-90,180,90],languages=['en']) 
+                #twitterStream.filter(languages=['en'])    
+            except:
+                #c.shutdown()           
+                print traceback 
+                print traceback.format_exc()
+                print sys.exc_info()[0]     
+                print 'sleeping......'
+                time.sleep(1)
+                pass   
+                #log(traceback.format_exc())
+                #log(sys.exc_info()[0])
+    
+    
+s = StreamingScraper()
+s.start()
+
+s.join()
+
+
+
+
+
+                        
+                        
+                        
